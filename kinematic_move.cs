56303524eec493149e166877599c4f98adcc5f72
Julio Iván Carrasco Armas <72397749+julio-carrasco@users.noreply.github.com>
2023-10-12 18:33:37 +0100

    Add files via upload

--- kinematic_move.cs
+++ kinematic_move.cs
@@ -1,15 +1,12 @@
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class kinematic_move : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 2000.0f;
-
-    private Rigidbody rb;
+    private float speed = 2.0f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -17,24 +14,24 @@ public class movement : MonoBehaviour
         float horizontalInput = 0f;
         float verticalInput = 0f;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             horizontalInput = -1.0f;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             horizontalInput = 1.0f;
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             verticalInput = 1.0f;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             verticalInput = -1.0f;
         }
 
         Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
-        rb.velocity = movement * speed * Time.deltaTime;
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
